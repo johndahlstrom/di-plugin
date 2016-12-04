9e0609ba3ddf8afb99f48da654c40bcc128f6269
John Dahlström <john@johndahlstrom.se>
2016-12-04 22:24:16 +0100

    corrected an error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var d = document;
 var wall = d.getElementsByClassName('paywall-content')[0];
 var blur = d.getElementsByClassName('paywall-mask')[0];
-wall.style.display = 'none';
+wall.style.height = 'auto';
 blur.style.height = 'auto';
