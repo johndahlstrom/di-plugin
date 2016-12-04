84dd4fc9c196ff1db3f845f6641b80062e15dfad
John Dahlström <john@johndahlstrom.se>
2016-12-04 22:12:04 +0100

    Changed the elements to work for the updated site

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-var d = document.getElementsByName('contentFrame')[0].contentWindow.document;
-var wall = d.getElementById('serviceplusPaywallpaywall-container');
-var wall2 = d.getElementsByClassName('paywall-content')[0];
+var d = document;
+var wall = d.getElementsByClassName('paywall-content')[0];
 var blur = d.getElementsByClassName('paywall-mask')[0];
 wall.style.display = 'none';
-wall2.style.height = 'auto';
 blur.style.height = 'auto';
